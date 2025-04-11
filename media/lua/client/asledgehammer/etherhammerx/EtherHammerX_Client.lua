901a17be01b478e51bc2c0fa3b6b9583181fdb43
JabDoesThings <JabDoesThings@github.com>
2025-04-11 13:26:09 -0500

    Added logo media button to escape menu.

--- media/lua/client/asledgehammer/etherhammerx/EtherHammerX_Client.lua
+++ media/lua/client/asledgehammer/etherhammerx/EtherHammerX_Client.lua
@@ -7,12 +7,71 @@
 local ModLoader = require 'asledgehammer/modloader/ModLoader';
 local ZedCrypt = require 'asledgehammer/encryption/ZedCrypt';
 
+require 'OptionScreens/MainScreen';
+
 local mod = 'EtherHammerX';
 local info = function(msg)
     print('[' .. mod .. '] :: ' .. msg);
 end
 
+local function lerp(start, stop, percent)
+    return (start + percent * (stop - start));
+end
+
 (function()
+    local logo = getTexture('media/ui/ehx_logo_transparent.png');
+    local old_render = MainScreen.render;
+    local old_onMouseDown = MainScreen.onMouseDown;
+
+    local core = getCore();
+
+    local function getLogoBoundary()
+        local x1, y1 = core:getScreenWidth() - 256, 0;
+        local x2, y2 = x1 + 256, 105;
+        return x1, y1, x2, y2;
+    end
+
+    local inside_logo = 0;
+    local logo_steps = 5;
+    MainScreen.render = function(self)
+        -- Call original method.
+        old_render(self);
+
+        local mx, my = getMouseX(), getMouseY();
+        local alpha = 0.75;
+        local lx1, ly1, lx2, ly2 = getLogoBoundary();
+        if lx1 <= mx and mx <= lx2 and ly1 <= my and my <= ly2 then
+            if inside_logo < logo_steps then
+                inside_logo = logo_steps;
+            end
+        else
+            if inside_logo > 0 then
+                inside_logo = inside_logo - 1;
+            end
+        end
+
+        if inside_logo ~= 0 then
+            alpha = lerp(alpha, 1.0, (inside_logo / logo_steps));
+        end
+
+        -- Render our stuff.
+        -- Logo:
+        --   100%: 1024 x 420
+        --    25%:  256 x 105
+        self:drawTextureScaledUniform(logo, lx1, ly1, 0.25, alpha, 1, 1, 1);
+    end
+
+    MainScreen.onMouseDown = function(self, mx, my)
+        -- Call original method.
+        old_onMouseDown(self, mx, my);
+
+        -- Check and see if the player clicked on the logo.
+        local lx1, ly1, lx2, ly2 = getLogoBoundary();
+        if lx1 <= mx and mx <= lx2 and ly1 <= my and my <= ly2 then
+            openUrl('https://github.com/asledgehammer/EtherHammerX/');
+        end
+    end
+
     local onGameStart = function()
         ModLoader.requestServerFile('EtherHammerX', 'client', function(result, data)
             if result == ModLoader.RESULT_FILE_NOT_FOUND then
@@ -22,5 +81,6 @@ end
             loadstring(ZedCrypt.decrypt(data, '__EtherHammerX__'))();
         end);
     end
+    LuaEventManager.AddEvent('OnLuaNetworkConnected');
     Events.OnLuaNetworkConnected.Add(onGameStart);
 end)();
